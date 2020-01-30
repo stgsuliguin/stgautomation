0e1bebadbcb3170c496e36b797a9356687a13f5b
stgsuliguin <stgsuliguin1@gmail.com>
2020-01-29 16:58:39 -0800

    test
    test

--- pytest/environment.py
+++ pytest/environment.py
@@ -149,7 +149,8 @@ duts = {'mp1a': {
     },
     'evolve': {
     'account': '16',
-    'wired_ip_address': '10.0.0.105',
+#    'wired_ip_address': '10.0.0.105',
+    'wired_ip_address': '192.168.1.36',
     'wireless_ip_address': 'mphome',
     'device_serial_number': 'AREYDC07023L',
     'android_serial_number': '40104543000014A3',
